b9b998561175e30b00549932f8b613c318d25ec8
Shrikar Archak <shrikar84@gmail.com>
2011-07-31 19:50:26 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopcircuit::Application.config.secret_token = 'dd4f7506010c4a33157d996f460db94320e10ffd2ecf53aa9fb403fb4a0cc18c68c5239805c12ae2dc35fd1e0e523a879bad3580a5f090d891f3a5ee7b2e1b7f'
