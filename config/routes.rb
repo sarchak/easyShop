fda00eb050fa9e0382f2583842d8562167659449
Shrikar Archak <shrikar84@gmail.com>
2011-07-31 20:43:57 -0700

    Added Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Shopcircuit::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/help"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
