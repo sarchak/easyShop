6310ba45a28f1f657c2f9dbf913d53cd55a57b87
Shrikar Archak <shrikar84@gmail.com>
2011-07-31 21:47:00 -0700

    v1 code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,19 @@
 class PagesController < ApplicationController
+  
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def help
+    @title = "Help"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,28 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Shopcircuit | Home")
+    end
+
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+      :content => "Shopcircuit | About")
     end
   end
 
@@ -21,12 +31,22 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+      :content => "Shopcircuit | Help")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+      :content => "Shopcircuit | Contact")
     end
   end
 
