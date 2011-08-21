c1504408f9580451ca8ad2c152288009bb7cdaf6
Shrikar Archak <shrikar84@gmail.com>
2011-08-21 13:09:44 -0700

    More code for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Shopcircuit::Application.routes.draw do
-  resources :users
+  get "sessions/new"
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
+  post "users/new"
   get "users/new"
   get "users/show"
   match '/signup', :to => 'users#new'
